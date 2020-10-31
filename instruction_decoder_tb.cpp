d810e3d80c8625c03d98f589708c3d0670a23810
Janis Dancis <gazz@digihaze.com>
2020-10-30 21:23:03 -0600

    decoder testbench

---
+++ instruction_decoder_tb.cpp
@@ -0,0 +1,154 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "Vinstruction_decoder.h"
+#include "verilated_vcd_c.h"
+#include "verilated.h"
+
+void	tick(int tickcount, Vinstruction_decoder *tb, VerilatedVcdC* tfp) {
+	tb->eval();
+	if (tfp)
+		tfp->dump(tickcount * 10 - 2);
+	tb->i_clk = 1;
+	tb->eval();
+	if (tfp)
+		tfp->dump(tickcount * 10);
+	tb->i_clk = 0;
+	tb->eval();
+	if (tfp) {
+		tfp->dump(tickcount * 10 + 5);
+		tfp->flush();
+	}
+	printf("i_we: %d, i_en: %d, o_ack: %d, o_set_mode: %d, o_set_pixel: %x\n", 
+		tb->i_we, tb->i_en, tb->o_ack, tb->o_set_mode, tb->o_set_pixel);
+}
+
+void set_display_mode(unsigned &tickcount, Vinstruction_decoder *tb, VerilatedVcdC* tfp);
+
+void set_pixel_color(unsigned &tickcount, Vinstruction_decoder *tb, VerilatedVcdC* tfp);
+
+
+int main(int argc, char **argv) {
+	unsigned tickcount = 0;
+
+	// Call commandArgs first!
+	Verilated::commandArgs(argc, argv);
+
+	// Instantiate our design
+	Vinstruction_decoder *tb = new Vinstruction_decoder;
+
+	// Generate a trace
+	Verilated::traceEverOn(true);
+	VerilatedVcdC* tfp = new VerilatedVcdC;
+	tb->trace(tfp, 99);
+	tfp->open("instruction_decoder.vcd");
+
+// i_clk, i_we, i_en, i_data, o_ack,
+// 	// signal generator instructions
+// 	// pixel generator instructions
+// 	);
+
+	tb->i_we = 1;
+	tb->i_en = 1;
+	tb->i_data = 0;
+	tick(++tickcount, tb, tfp);
+	tick(++tickcount, tb, tfp);
+	tick(++tickcount, tb, tfp);
+
+
+	set_display_mode(tickcount, tb, tfp);
+
+	set_pixel_color(tickcount, tb, tfp);
+
+
+}
+
+
+void set_display_mode(unsigned &tickcount, Vinstruction_decoder *tb, VerilatedVcdC* tfp) {
+	// set display mode
+	tb->i_we = 0;
+	tb->i_data = 1;
+	tick(++tickcount, tb, tfp);
+
+	tb->i_en = 0;
+	tick(++tickcount, tb, tfp);
+
+	tb->i_en = 1;	
+	tick(++tickcount, tb, tfp);
+
+	tb->i_data = 11;
+	tick(++tickcount, tb, tfp);
+
+	tb->i_en = 0;
+	tick(++tickcount, tb, tfp);
+
+	tb->i_en = 1;	
+	tick(++tickcount, tb, tfp);
+
+	tb->i_we = 1;
+	tick(++tickcount, tb, tfp);
+	tick(++tickcount, tb, tfp);
+	tick(++tickcount, tb, tfp);
+	tick(++tickcount, tb, tfp);
+	tick(++tickcount, tb, tfp);
+	// it takes 5 clocks for the result
+	printf("Set Mode: %d, Mode: %d\n", tb->o_set_mode, tb->o_mode);
+	tick(++tickcount, tb, tfp);
+	printf("Set Mode: %d, Mode: %d\n", tb->o_set_mode, tb->o_mode);
+	tick(++tickcount, tb, tfp);
+	tick(++tickcount, tb, tfp);
+	tick(++tickcount, tb, tfp);
+
+	// instruction done and all quiet
+	tick(++tickcount, tb, tfp);
+	printf("Set Mode: %d, Mode: %d\n", tb->o_set_mode, tb->o_mode);
+}
+
+void set_pixel_color(unsigned &tickcount, Vinstruction_decoder *tb, VerilatedVcdC* tfp) {
+	// set display mode
+	tb->i_we = 0;
+	tb->i_data = 2;
+	tick(++tickcount, tb, tfp);
+
+	tb->i_en = 0;
+	tick(++tickcount, tb, tfp);
+
+	tb->i_en = 1;	
+	tick(++tickcount, tb, tfp);
+
+	// next 12 bits are color LSB
+	tb->i_data = 0x12;
+	tb->i_en = 0;
+	tick(++tickcount, tb, tfp);
+
+	tb->i_en = 1;	
+	tick(++tickcount, tb, tfp);
+
+	tb->i_data = 0x34;
+	tb->i_en = 0;
+	tick(++tickcount, tb, tfp);
+
+	tb->i_en = 1;	
+	tick(++tickcount, tb, tfp);
+
+	tb->i_we = 1;
+	tick(++tickcount, tb, tfp);
+	tick(++tickcount, tb, tfp);
+	tick(++tickcount, tb, tfp);
+	tick(++tickcount, tb, tfp);
+	tick(++tickcount, tb, tfp);
+	// it takes 5 clocks for the result
+	// 	o_pixel_x, o_pixel_y, o_color, o_set_pixel
+	printf("Set Pixel: %d, X: %d, Y: %d, color: %x\n", 
+		tb->o_set_pixel, tb->o_pixel_x, tb->o_pixel_y, tb->o_color);
+	tick(++tickcount, tb, tfp);
+	tick(++tickcount, tb, tfp);
+	tick(++tickcount, tb, tfp);
+	tick(++tickcount, tb, tfp);
+	tick(++tickcount, tb, tfp);
+
+	// instruction done and all quiet
+	tick(++tickcount, tb, tfp);
+
+	printf("Set Pixel: %d, X: %d, Y: %d, color: %x\n", 
+		tb->o_set_pixel, tb->o_pixel_x, tb->o_pixel_y, tb->o_color);
+}
